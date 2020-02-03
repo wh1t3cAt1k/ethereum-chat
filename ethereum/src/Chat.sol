455c38b2f7a7dcc925c1b2563cdf01fd9f4adf31
Pavel Kabir <pavel@kabir.ru>
2020-02-03 20:36:50 +0300

    Implement basic message contract

--- ethereum/src/Chat.sol
+++ ethereum/src/Chat.sol
@@ -1,6 +1,23 @@
 pragma solidity ^0.6.2;
 
 contract AnonymousChat {
-    function sendMessage(string memory message) public {
+    struct Message {
+        uint256 dateTime;
+        address senderAddress;
+        string senderName;
+        string text;
+    }
+
+    Message[] public messages;
+
+    function sendMessage(string memory senderName, string memory text) public {
+        Message memory message = Message({
+            dateTime: block.timestamp,
+            senderAddress: msg.sender,
+            senderName: senderName,
+            text: text
+        });
+
+        messages.push(message);
     }
 }
